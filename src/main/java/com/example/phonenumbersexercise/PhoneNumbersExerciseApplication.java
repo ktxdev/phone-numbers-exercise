129f85373de5d0f58191a217ae597144923f8130
ktxdev <sean.ktxdev@gmail.com>
2022-04-11 19:50:20 +0200

    Initial Commit

---
+++ src/main/java/com/example/phonenumbersexercise/PhoneNumbersExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonenumbersexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumbersExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneNumbersExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phonenumbersexercise/PhoneNumbersExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonenumbersexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumbersExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
